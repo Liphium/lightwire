2df319b28234f0c3ab5f8d4aca79065c04eeeabd
Julian <unfehlerhaft@gmail.com>
2025-02-24 19:19:00 +0100

    Some simple audio recording

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use cpal::traits::{DeviceTrait, HostTrait};
+use std::time::Duration;
+
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 fn main() {
     println!("Hello, world!");
 
-    println!("Supported hosts: {:?}", cpal::ALL_HOSTS);
     let host = cpal::default_host();
-    let device = host.default_output_device().unwrap();
-    println!("using device: {:?}", device.name().unwrap());
+    let device = host.default_input_device().unwrap();
+
+    println!(
+        "using {} with device {}",
+        host.id().name(),
+        device.name().unwrap()
+    );
+
+    let config = device
+        .default_input_config()
+        .expect("Failed to get default input config");
+
+    let mic_config = cpal::StreamConfig {
+        buffer_size: cpal::BufferSize::Default,
+        channels: config.channels(),
+        sample_rate: config.sample_rate(),
+    };
+
+    let err_fn = move |err| eprintln!("error in cpal: {}", err);
+
+    let stream = device
+        .build_input_stream(&mic_config, move |data, _| handle_data(data), err_fn, None)
+        .expect("Couldn't start input stream");
+
+    stream.play().expect("Couldn't play stream");
+    std::thread::sleep(Duration::from_secs(3));
+}
+
+fn handle_data(samples: &[f32]) {
+    println!("received with length: {}", samples.len())
 }
